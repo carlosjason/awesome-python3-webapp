34ccec160113ad91c1a6e9cd15ccef6684bd1fe0
carlosjason <lixi0106@gmail.com>
2016-11-27 20:53:19 +0800

    day 5

--- www/apis.py
+++ www/apis.py
@@ -2,13 +2,37 @@ import json,logging, inspect, functools
 
 
 class APIError(Exception):
-    pass
+    '''
+    the base APIError which contains error(required), data(optional), and message(optional).
+    '''
+
+    def __init__(self, error, data ='', message = ''):
+        super(APIError.self).__init__(message)
+        self.error = error
+        self.data = data
+        self.message = message
 
 class APIValueError(APIError):
-    pass 
+    '''
+    Indicate the input value has error or invalid, the data specifies the error field of input form.
+    '''
+    def __init__(self, field, message =''):
+        super(APIValueError,self).__init__('value:invalid', field,message) 
+
 
 class APIResourceNotFoundError(APIError):
-    pass 
+    '''
+    Indicate the resource was not found. The data specifies resource name.    
+    '''    
+    def __init__(self, field, message =''):
+        super(APIResourceNotFoundError,self).__init__('Value not found', field, message)
 
 class APIPermissionError(APIError):
-    pass
+    '''
+    Indicate the api has no permission
+    '''
+    def __init__(self,message =''):
+        super(APIPermissionError,self).__init__('Permission forbidden', 'permission', message)
+    
+    
+    
